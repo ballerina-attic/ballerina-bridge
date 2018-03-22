2569b5e590d6b90d738aaf8745e7c8b8d5683f56
daneshk <dknkuruppu@gmail.com>
2018-03-23 02:29:10 +0530

    Refactor the code. move to another package

--- samples/spring-boot-samples/declarative-transaction-sample/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-drop table BOOKINGS if exists;
-create table BOOKINGS(ID serial, FIRST_NAME varchar(20) NOT NULL);
-
-drop table ROOMS if exists;
-create table ROOMS (ID serial, ROOM_NO INT NOT NULL);
